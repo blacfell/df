20afd08d2c62bd009f637dda8c8c68352aea1895
kitty fangz <kittycatfangz@gmail.com>
2021-12-24 12:30:15 +1100

    fixed nord theme not loading when emacs is run as daemon

--- .emacs.d/el/look.el
+++ .emacs.d/el/look.el
@@ -3,6 +3,15 @@
 ;; i usually remove the menu bar but i feel like keeping it right now
 (setq visible-bell t)
 
+;; i took this from reddit
+(defun meow/load-theme-for-frame (frame)
+  "Load theme when new frame is created. Needed for emacsdaemon."
+  (select-frame frame)
+  (load-theme 'nord t))
+
 (use-package nord-theme
   :config
-  (load-theme 'nord t))
+  ;; this whole thing is needed to make nord work when emacs is a daemon
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions #'meow/load-theme-for-frame)
+    (load-theme 'nord t)))
